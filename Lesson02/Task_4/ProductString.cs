a3293b39f5d639e624774b3f6bdc3dee1103c047
UShandruk <ulyana.shandruk@gmail.com>
2021-10-06 18:46:34 +0300

    Остатки от пул реквеста с ДЗ к Lesson02

---
+++ Lesson02/Task_4/ProductString.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    /// <summary>
+    /// Запись о товаре
+    /// </summary>
+    public class ProductString
+    {
+        /// <summary>
+        /// Уникальный идентификатор товара
+        /// </summary>
+        public int Id;
+
+        /// <summary>
+        /// Наименование
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// Цена
+        /// </summary>
+        public double Price;
+
+        /// <summary>
+        /// Количество
+        /// </summary>
+        public int Quantity;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public ProductString(int id, string name, double price, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
