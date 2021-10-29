2fd733b8a18cfaf96613fbc90816cf79bc81a2ac
UShandruk <u.shandruk@yusar.ru>
2021-10-29 10:43:29 +0300

    Алгоритмы и структуры данных - ДЗ-01

---
+++ 2_Algorithms-and-data-structures/Lesson01/Task1/Task2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    // 2. Посчитайте сложность функции
+    // Вычислите асимптотическую сложность функции из примера ниже.
+
+    public class Task2
+    {
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0; // O(1)
+            for (int i = 0; i < inputArray.Length; i++) // O(i(N))
+            {
+                for (int j = 0; j < inputArray.Length; j++) // O(j(N))
+                {
+                    for (int k = 0; k < inputArray.Length; k++) //O(k(N))
+                    {
+                        int y = 0;
+                        if (j != 0)
+                        {
+                            y = k / j;// O(1)
+                        }
+                        sum += inputArray[i] + i + k + j + y;// O(1)
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
+// Асимптотическая сложность:
+// O(i(N)) * O(j(N)) * O(k(N)) = O(N3), т.е. O (N в кубе),
+// т.к. в методе есть три цикла, вложенных друг в друга.
---
+++ 2_Algorithms-and-data-structures/Lesson01/Task3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Task3
+{
+    //3. Реализуйте функцию вычисления числа Фибоначчи
+    //Требуется реализовать рекурсивную версию и версию без рекурсии(через цикл).
+    //Пример чисел Фибоначчи:
+    //F(0) = 0,
+    //F(1) = 1.
+    //Для остальных чисел:
+    //F(N) = F(N-2) + F(N-1).
+    //То есть для F(2) будет F(2) = F(0) + F(1) = 0 + 1 = 1.
+    //F(3) будет F(3) = F(1) + F(2) = 1 + 1 = 2.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = 1;
+            string result = "";
+            while (true)
+            {
+                Console.WriteLine("Сколько чисел должно быть в ряде Фибоначчи?");
+                try
+                {
+                    userInput = Convert.ToInt32(Console.ReadLine());
+                    if (userInput > 0)
+                    {
+                        int[] row = calcFibonacciRow(userInput);
+                        foreach (int i in row)
+                        {
+                            result = result + i.ToString() + " ";
+                        }
+                        Console.WriteLine(result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введите целое положительное число");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введите целое положительное число");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Вычислить ряд Фибоначчи (0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ...)
+        /// </summary>
+        /// <param name="number">Количество чисел в ряду (целое положительное число)</param>
+        private static int[] calcFibonacciRow(int number)
+        {
+            int[] row = new int[number];
+            row[0] = 0;
+            row[1] = 1;
+
+            for (int i = 2; i < number; ++i)
+            {
+                row[i] = row[i - 1] + row[i - 2];
+            }
+
+            return row;
+        }
+    }
+}
